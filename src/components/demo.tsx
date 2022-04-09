f9b1cf2291994ffb3731f3d1081d68ce476a4133
Kronos <1963024305@qq.com>
2022-04-09 15:55:02 +0800

    初始化

---
+++ src/components/demo.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+//React FC 写法 推荐写这种
+const demo: React.FC<{}> = () => {
+
+    return (
+        <div style={{width:'100%', height:'100%'}}>
+            demo
+       </div>);
+};
+
+//React Component 写法
+// import React, {Component} from 'react'
+
+// class demo extends Component<any, any> {
+//     constructor(props: any){
+//         super(props);
+//     }
+
+//     render(){
+//         return(
+//             <div style={{width:'100%', height:'100%'}}></div>
+//         )
+//     }
+// }
+
+export default demo
