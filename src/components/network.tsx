c75b8fe9b655a6c7c700edd835c4c7a93a3dd6e6
Kronos <1963024305@qq.com>
2022-05-10 13:36:51 +0800

    tijiao

--- src/components/network.tsx
+++ src/components/network.tsx
@@ -1,6 +1,6 @@
-import React, { useEffect, useState } from "react";
+import React, { Component, useEffect, useState } from "react";
 import { getNetWorkByCommunity } from "../api/networkApi";
-import ForceGraph from "force-graph";
+import ForceGraph, { GraphData } from "force-graph";
 import ForceGraph3D from '3d-force-graph'
 
 type NetworkState = {
@@ -8,20 +8,22 @@ type NetworkState = {
 }
 
 //React FC 写法 推荐写这种
-const Network: React.FC<{}>= () => {
+const Network: React.FC<{}> = () => {
 
   const [NetworkState, setNetworkState] = useState<NetworkState>({
     container: React.createRef()
   });
 
   const getData = () => {
-    let data = getNetWorkByCommunity(1834615);
+    let data = getNetWorkByCommunity(1937735);
     return new Promise((resolve, reject) => {
       resolve(data);
     });
   }
 
+  const linkColor = ['rgba(0,0,0,0.2)','rgba(255,255,255,0.5)'];
   useEffect(() => {
+    console.log(1);
     getData().then((dataset)=> {
       console.log(dataset);
       const {container} = NetworkState;
@@ -29,23 +31,25 @@ const Network: React.FC<{}>= () => {
       if(container.current != null){
         //@ts-ignore
         myGraph(container.current ).graphData(dataset)
-        .backgroundColor('#101020')
+        // .backgroundColor('#101020')
+        .backgroundColor('rgba(255,255,255,0.5)')
+        .width(1300)
+        .height(800)
         .nodeRelSize(6)
+        // .zoom(1)
         //@ts-ignore
         // .nodeColor((node)=>node.color)
         .nodeAutoColorBy("weight")
-        .linkColor(() => 'rgba(255,255,255,0.2)')
+        .linkColor(() => linkColor[0])
         .onNodeClick(node=>{
           console.log(node);
         });
-        
+
       }
     });
   }, []);
 
-  return (<div>
-    <div ref={NetworkState.container} style={{ width: "100vw", height: "100vh" }}></div>
-  </div>)
+  return (<div ref={NetworkState.container} style={{ width: "100%", height: "100%" }}></div>)
 };
 
 export default Network;
