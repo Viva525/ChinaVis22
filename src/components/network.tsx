ee4d58cdbdefc77008dba4b96c51f01324547fab
Stephenzcc <401186943jrf@gmail.com>
2022-05-10 13:38:54 +0800

    1

--- src/components/network.tsx
+++ src/components/network.tsx
@@ -1,17 +1,16 @@
 import React, { useEffect, useState } from "react";
 import { getNetWorkByCommunity } from "../api/networkApi";
 import ForceGraph from "force-graph";
-import ForceGraph3D from '3d-force-graph'
+import ForceGraph3D from "3d-force-graph";
 
 type NetworkState = {
-  container: any
-}
+  container: any;
+};
 
 //React FC 写法 推荐写这种
-const Network: React.FC<{}>= () => {
-
+const Network: React.FC<{}> = () => {
   const [NetworkState, setNetworkState] = useState<NetworkState>({
-    container: React.createRef()
+    container: React.createRef(),
   });
 
   const getData = () => {
@@ -19,33 +18,39 @@ const Network: React.FC<{}>= () => {
     return new Promise((resolve, reject) => {
       resolve(data);
     });
-  }
+  };
 
   useEffect(() => {
-    getData().then((dataset)=> {
+    getData().then((dataset) => {
       console.log(dataset);
-      const {container} = NetworkState;
+      const { container } = NetworkState;
       const myGraph = ForceGraph3D();
-      if(container.current != null){
+      if (container.current != null) {
         //@ts-ignore
-        myGraph(container.current ).graphData(dataset)
-        .backgroundColor('#101020')
-        .nodeRelSize(6)
-        //@ts-ignore
-        // .nodeColor((node)=>node.color)
-        .nodeAutoColorBy("weight")
-        .linkColor(() => 'rgba(255,255,255,0.2)')
-        .onNodeClick(node=>{
-          console.log(node);
-        });
-        
+        myGraph(container.current)
+          //@ts-ignore
+          .graphData(dataset)
+          .backgroundColor("#101020")
+          .nodeRelSize(6)
+          //@ts-ignore
+          // .nodeColor((node)=>node.color)
+          .nodeAutoColorBy("weight")
+          .linkColor(() => "rgba(255,255,255,0.2)")
+          .onNodeClick((node) => {
+            console.log(node);
+          });
       }
     });
   }, []);
 
-  return (<div>
-    <div ref={NetworkState.container} style={{ width: "100vw", height: "100vh" }}></div>
-  </div>)
+  return (
+    <div>
+      <div
+        ref={NetworkState.container}
+        style={{ width: "100vw", height: "100vh" }}
+      ></div>
+    </div>
+  );
 };
 
 export default Network;
