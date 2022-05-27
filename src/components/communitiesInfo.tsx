708275d9f1908c22300928d9b4d5c85dc803a5ad
ashley <504325906@qq.com>
2022-05-27 22:09:27 +0800

    11

--- src/components/communitiesInfo.tsx
+++ src/components/communitiesInfo.tsx
@@ -7,9 +7,9 @@ const CommunitiesInfo: React.FC<{}> = () => {
     const height = +svg.attr('height');
     svg.attr("viewBox", [0, 0, width, height]);
     const g = svg.append('g')
-        .attr('transform', `translate(${width / 2}, ${height / 2})`);
-    let color = d3.scaleOrdinal(d3.schemeCategory10);
+        .attr('transform', `translate(${width / 2}, ${height / 2})`)
     let root;
+    let color = d3.scaleOrdinal(d3.schemeCategory10);
 
     //画一段圆弧
 
@@ -42,39 +42,66 @@ const CommunitiesInfo: React.FC<{}> = () => {
     //const darc=d3.arc().startAngle(d=>(d.x0+d.x1*i/9)).endAngle(d=>(d.x0+d.x1*(i+1)/9)).innerRadius(d=>d.y1+240).outerRadius(d=>(d.y1+240+i*20));
     const darc = d3.arc().startAngle((d: any) => (d.x0 + d.x1 * 6 / 9)).endAngle((d: any) => (d.x0 + d.x1 * (6 + 1) / 9)).innerRadius((d: any) => d.y1 + 240).outerRadius((d: any) => (d.y1 + 240 + 1 * 20));
     const fill = (d: any) => {
+        // if (d.depth === 0)
+        //     return color(d.data.name)
+        // // while (d.depth > 1) 
+        // //     d = d.parent; 
+        // // return color(d.data.name); 
+        // else if (d.depth>1)
+        //     return color(d.data.name)
         return color(d.data.name)
     }
 
-    const render = function (data) {
-
-        let relations = data.descendants().filter(d => d.depth == 3)
+    const render = function (data: any) {
+        let relations = data.descendants().filter((d: any) => d.depth == 3)
+        let Nodes = data.descendants().filter((d: any) => d.depth == 2)
+        let All_N = []
         let All_r = []
-        relations.forEach(element => {
+        relations.forEach((element: any) => {
             All_r.push(element.data.weight)
         });
+        Nodes.forEach((element: any) => {
+            All_N.push(element.data.weight)
+        });
+        console.log(All_N)
         let Max_r = Math.max.apply(null, All_r)
+        let Max_N = Math.max.apply(null, All_N)
 
-        g.selectAll('.datapath1').data(data.descendants().filter(d => d.depth == 2 || d.depth == 1)).join('path') //去掉根节点
+        g.selectAll('.datapath1').data(data.descendants().filter((d: any) => (d.depth == 1))).join('path') //去掉根节点
             .attr('class', 'datapath1')
             .attr('d', d3.arc().startAngle((d: any) => d.x0).endAngle((d: any) => d.x1).innerRadius((d: any) => d.y0).outerRadius((d: any) => d.y1))
             .attr('fill', fill)
+        //debugger;
+        //Domain、Cert、IP层
+        g.selectAll('.datapath5').data(data.descendants().filter((d: any) => (d.depth == 2))).join('path') //去掉根节点
+            .attr('class', 'datapath5')
+            .attr('d', d3.arc().startAngle((d: any) => d.x0).endAngle((d: any) => d.x1).innerRadius((d: any) => (d.y1 - d.y0) * (d.data.weight / Max_N) + d.y0).outerRadius((d: any) => d.y1))
+            .attr('fill', fill)
 
-        g.selectAll('.datapath2').data(data.descendants().filter(d => d.depth == 3)).join('path')
+        //关系层
+        g.selectAll('.datapath2').data(data.descendants().filter((d: any) => d.depth == 3)).join('path')
             .attr('class', 'datapath2')
             .attr('d', d3.arc().startAngle((d: any) => d.x0).endAngle((d: any) => d.x1).innerRadius((d: any) => d.y0).outerRadius((d: any) => (d.y1 - d.y0) * (d.data.weight / Max_r) + d.y0))
             .attr('fill', fill)
             .attr('opacity', 0.5)
 
-        g.selectAll('.datapath3').data(data.descendants().filter((d: any) => d.depth == 3)).join('path') //去掉根节点
+        g.selectAll('.datapath3').data(data.descendants().filter(d => d.depth == 3)).join('path') //去掉根节点
             .attr('class', 'datapath3')
             .attr('d', d3.arc().startAngle((d: any) => d.x0).endAngle((d: any) => d.x1).innerRadius((d: any) => d.y1).outerRadius((d: any) => (d.y1 + 10)))
             .attr('fill', fill)
 
-        g.selectAll('.datapath4').data(data.descendants().filter(d => (d.depth == 2 && d.data.name == 'Domain'))).join('path') //去掉根节点
+        g.selectAll('.datapath4').data(data.descendants().filter((d: any) => (d.depth == 2 && d.data.name == 'Domain'))).join('path') //去掉根节点
             .attr('class', 'datapath4')
             .attr('d', d3.arc().startAngle((d: any) => d.x0).endAngle((d: any) => d.x1).innerRadius((d: any) => d.y1 + 90).outerRadius((d: any) => (d.y1 + 100))).attr('fill', fill);
 
-        g.selectAll('.datatext').data(data.descendants().filter(d => d.depth == 2))
+        // for(i=0;i<9;i++){
+        //   g.selectAll('.darc'+i.toString()).data(data.descendants().filter(d=>(d.depth==1&&d.data.name=='Domain'))).join('path') //去掉根节点
+        //   .attr('class','darc'+i.toString())
+        //   .attr('d',d3.arc().startAngle(d=>(d.x0+d.x1*i/9)).endAngle(d=>(d.x0+d.x1*(i+1)/9)).innerRadius(d=>d.y1+130).outerRadius(d=>(d.y1+130+i*20)))
+        //   .attr('fill',fill);
+        // }
+
+        g.selectAll('.datatext').data(data.descendants().filter((d: any) => d.depth == 2))
             .join('text')
             .attr('class', 'datatext')
             .attr('text-anchor', 'middle')
@@ -88,7 +115,7 @@ const CommunitiesInfo: React.FC<{}> = () => {
             .text((d: any) => d.data.name)
         //dy属性
 
-        g.selectAll('.datatext_center').data(data.descendants().filter(d => d.depth == 1))
+        g.selectAll('.datatext_center').data(data.descendants().filter((d: any) => d.depth == 1))
             .join('text')
             .attr('class', 'datatext_center')
             .attr('text-anchor', 'middle')
@@ -111,7 +138,7 @@ const CommunitiesInfo: React.FC<{}> = () => {
     }
 
 
-    const bar = function (data, typeNum, j) {
+    const bar = function (data: any, typeNum: any, j: any) {
         let maxNum = Math.max.apply(null, typeNum)
         console.log(data)
         for (let i = 0; i < 9; i++) {
@@ -123,6 +150,34 @@ const CommunitiesInfo: React.FC<{}> = () => {
     }
 
 
+
+
+
+    // d3.json('static/data/games.json').then( data => {
+    //   root = d3.partition().size([2 * Math.PI, height / 1.6/1.8])//height变成圆周的一圈，width要变成sunburst最长的半径（自己调）
+    //   (d3.hierarchy(data).sum(d => d.popularity)
+    //   .sort((a, b) => b.popularity - a.popularity));
+    //   //console.log(root)
+    //   render(root);
+    //   data=root.descendants().filter(d=>(d.depth==2&&d.data.name=='Domain'));
+    //   console.log(data)
+    //   let typeNum=[[1,4,3,5,7,8,6,9,10],[2,3,3,7,7,8,6,9,10],[3,3,3,7,7,8,6,9,10]]
+
+    //   // d=[]
+    //   // d.push(data[0])
+    //   // bar(d,typeNum[0],0);
+    //   // d=[]
+    //   // d.push(data[1])
+    //   // bar(d,typeNum[1],1);
+
+    //   for(m=0;m<typeNum.length;m++){
+    //     console.log(m);
+    //     d=[]
+    //     d.push(data[m])
+    //     bar(d,typeNum[m],m);
+    //   }
+
+    // });
     let data = {
         "name": "Community",
         "children": [
@@ -419,6 +474,13 @@ const CommunitiesInfo: React.FC<{}> = () => {
         data = root.descendants().filter(d => (d.depth == 2 && d.data.name == 'Domain'));
         console.log(data)
 
+
+        // d=[]
+        // d.push(data[0])
+        // bar(d,typeNum[0],0);
+        // d=[]
+        // d.push(data[1])
+        // bar(d,typeNum[1],1);
 
         for (let m = 0; m < typeNum.length; m++) {
             console.log(m);
